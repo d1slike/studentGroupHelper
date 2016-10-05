44e31413033151130d06c7061a1a2dc4011e07cf
ykomissarov <eldustru@yandex.ru>
2016-10-05 20:09:52 +0300

    GM

---
+++ src/main/java/ru/disdev/entity/GMailMessage.java
@@ -0,0 +1,23 @@
+package ru.disdev.entity;
+
+public class GMailMessage {
+
+    private String emailAddress;
+    private String historyId;
+
+    public String getHistoryId() {
+        return historyId;
+    }
+
+    public void setHistoryId(String historyId) {
+        this.historyId = historyId;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+}
