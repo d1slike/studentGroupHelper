aca87f22f7ef253f3dd8f99912d504ae735bbb6a
d1slike <eldustru@yandex.ru>
2016-12-21 00:25:50 +0300

    *

--- src/main/java/ru/disdev/model/flows/files/TagSearchFlow.java
+++ src/main/java/ru/disdev/model/flows/files/TagSearchFlow.java
@@ -1,6 +1,8 @@
 package ru.disdev.model.flows.files;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import ru.disdev.bot.MessageConst;
 import ru.disdev.bot.TelegramKeyBoards;
 import ru.disdev.entity.FileFilter;
@@ -8,12 +10,16 @@ import ru.disdev.entity.Prototype;
 import ru.disdev.model.Action;
 import ru.disdev.model.StateActionMap;
 import ru.disdev.model.flows.Flow;
+import ru.disdev.service.TeacherService;
 
 @Prototype
 public class TagSearchFlow extends Flow<FileFilter> {
     public TagSearchFlow(long chatId) {
         super(chatId);
     }
+
+    @Autowired
+    private TeacherService service;
 
     @Override
     protected FileFilter buildResult() {
@@ -36,6 +42,8 @@ public class TagSearchFlow extends Flow<FileFilter> {
 
     @Override
     protected StateActionMap fillStateActions(StateActionMap map) {
+        ReplyKeyboardMarkup markup = TelegramKeyBoards.tagListKeyboard(service.getSubjectTags());
+        //todo
         return map.next(new Action(this::getFilter, "Выберите предмет", TelegramKeyBoards.cancelButton()));
     }
 }
