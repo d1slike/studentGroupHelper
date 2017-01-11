65e80f1abd8753b185bb7ac09ceb5dd490848fcd
ykomissarov <eldustru@yandex.ru>
2017-01-11 15:04:41 +0300

    *

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup java -Xmx512m -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar student_helper-1.0.0-SNAPSHOT.jar > log/stdout.log 2>1&
+nohup java -Xmx512m -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar student_helper-1.0.0-SNAPSHOT.jar > stdout.log 2>1&
