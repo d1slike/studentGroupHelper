79c768a31e530ff57bce2d375d61474659c5c8ed
ykomissarov <eldustru@yandex.ru>
2017-01-11 15:29:48 +0300

    deploy

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup java -Xmx512m -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar build/libs/student_helper-1.0.0-SNAPSHOT.jar > stdout.log 2>1&
+nohup java -Xmx512m -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar build/libs/student_helper-1.0.0-SNAPSHOT.jar > stdout.log &
